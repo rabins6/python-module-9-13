c95e40112cc5fc5c2c4f21bf61a365ec71cb7ecb
rabins6 <143177381+rabins6@users.noreply.github.com>
2024-11-27 15:34:08 +0200

    Add files via upload

---
+++ 11.1.py
@@ -0,0 +1,33 @@
+class Publication:
+    def __init__(self, name):
+        self.name = name
+
+class Book(Publication):
+    def __init__(self, name, author, page_count):
+        super().__init__(name)
+        self.author = author
+        self.page_count = page_count
+
+    def print_information(self):
+        print(f"Book Name: {self.name}")
+        print(f"Author: {self.author}")
+        print(f"Page Count: {self.page_count}")
+
+class Magazine(Publication):
+    def __init__(self, name, chief_editor):
+        super().__init__(name)
+        self.chief_editor = chief_editor
+
+    def print_information(self):
+        print(f"Magazine Name: {self.name}")
+        print(f"Chief Editor: {self.chief_editor}")
+
+# Main program
+if __name__ == "__main__":
+    magazine = Magazine("Donald Duck", "Aki Hyyppä")
+    book = Book("Compartment No. 6", "Rosa Liksom", 192)
+
+    print("Publication Information:\n")
+    magazine.print_information()
+    print("\n")
+    book.print_information()
